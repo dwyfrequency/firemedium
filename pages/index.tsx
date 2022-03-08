c9fcf36a68650334954fff86d436e197bc59f7eb
dwyfrequency <jdwy215@gmail.com>
2022-03-07 22:08:02 -0500

    insert navbar into page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import Loader from '../components/Loader';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
       <Link href={'todo'}>
         <a>merp</a>
       </Link>
+      <Loader show></Loader>
     </div>
   );
 };
