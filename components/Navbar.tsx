dd0a266fb6564eb0defd8fdf6edfa29414b505ce
dwyfrequency <jdwy215@gmail.com>
2022-03-07 22:07:46 -0500

    add navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { User } from '../lib/types';
+
+// Top navbar
+export default function Navbar() {
+  const user: User = {};
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/" passHref>
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin" passHref>
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`} passHref>
+                <Image src={user?.photoURL ?? ''} alt={'hello'} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter" passHref>
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
